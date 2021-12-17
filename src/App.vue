f39d40c7daa16853d18fd39f55c616b9501dd10e
Rodrigo Vergara <67773102+UnTalPeluca@users.noreply.github.com>
2021-12-17 04:44:11 -0300

    Finished Project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NFTCard 
+    :img="card.img" 
+    :name="card.name"
+    :id="card.id"
+    :description="card.description"
+    :price="card.price"
+    :time="card.time"
+    :autor="card.autor"
+    :avatar="card.avatar"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NFTCard from './components/NFTCard/NFTCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NFTCard
+  },
+  data: () => ({
+    card: {
+      img: 'image-equilibrium.jpg',
+      name: 'Equilibrium',
+      description: 'Our Equilibrium collection promotes balance and calm.',
+      id: 3429,
+      price: 0.041,
+      time: 3,
+      autor: 'Jules Wyvern',
+      avatar: 'image-avatar.png',
+    }
+  })
 }
 </script>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap');
+
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+html {
+  font-size: 62.5%;
+}
+img {
+  width: 100%;
+  vertical-align: middle;
+}
+body {
+  background-color: #0D192C;
+  font-family: 'Outfit', sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  margin-top: 62px;
+  @media screen and (min-width: 768px) {
+      margin-top: 152px;
+  }
 }
 </style>
---
+++ src/components/NFTCard/NFTCard.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="card">
+    <div class="card__product-img">
+      <img class="card__img" 
+        :src="require('@/static/images/' + img)"
+      >
+    </div>
+    <div class="card__content">
+      <p class="card__name">{{ name }} <span v-if="id">#{{ id }}</span></p>
+      <p class="card__description" v-if="description">{{ description }}</p>
+      <div class="card__content-bottom">
+        <div class="card__price">
+          <div class="icon">
+            <img src="@/assets/images/nft-card/icon-ethereum.svg">
+          </div>
+          <p>{{ price }} ETH</p>
+        </div>
+        <div v-if="time" class="card__time">
+          <div class="icon">
+            <img src="@/assets/images/nft-card/icon-clock.svg">
+          </div>
+          <p>{{ time }} days left</p>
+        </div>
+      </div>
+    </div>
+    <div class="card__footer">
+      <div class="card__avatar">
+        <img :src="require('@/static/images/' + avatar)">
+      </div>
+      <p class="card__autor">Creation of <span class="card__autor--alt-color">{{ autor }}</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      default: '',
+      required: true
+    },
+    name: {
+      type: String,
+      default: '',
+      required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: [String, Number],
+      default: null
+    },
+    price: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    time: {
+      type: [String, Number],
+      default: ''
+    },
+    autor: {
+      type: String,
+      default: ''
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    imgHovered: false,
+  })
+}
+</script>
+
+<style scoped lang="scss">
+.icon {
+  display: flex;
+}
+.card {
+  background-color: #15263F;
+  color: #8BACD9;
+  border-radius: 16px;
+  padding: 24px;
+  width: 327px;
+  font-size: 1.5rem;
+  box-shadow: 0 25px 50px 0 rgba(0,0,0,0.1);
+  @media screen and (min-width: 768px) {
+    font-size: 1.6rem;
+    width: 350px;
+    padding-bottom: 32px;
+  }
+  .card__product-img {
+    cursor: pointer;
+    position: relative;
+    border-radius: 8px;
+    overflow: hidden;
+    @mixin hoverOpacity {
+      content: "";
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      transition: opacity 0.25s ease-out;
+    }
+    &::before {
+      @include hoverOpacity;
+      background-color: cyan;
+    }
+    &::after {
+      @include hoverOpacity;
+      background: no-repeat url(../../assets/images/nft-card/icon-view.svg) center;
+    }
+    &:hover {
+      &::before {
+        opacity: 0.5;
+      }
+      &::after {
+        opacity: 1;
+      }
+    }
+  }
+  .card__content {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    padding: 24px 0 16px 0;
+    @media screen and (min-width: 768px) {
+      gap: 16px;
+      padding: 24px 0;
+    }
+    .card__name {
+      color: white;
+      font-size: 2.2rem;
+      font-weight: bold;
+      cursor: pointer;
+      &:hover {
+        color: #00FFF8;
+      }
+    }
+    .card__description {
+      font-size: 1.8rem;
+      line-height: 2.6rem;
+      font-weight: lighter;
+    }
+    .card__content-bottom {
+      display: flex;
+      justify-content: space-between;
+      padding-top: 4px;
+      > * {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        font-weight: 600;
+      }
+      .card__price {
+        color: #00FFF8;
+      }
+    }
+  }
+  .card__footer {
+    display: flex;
+    align-items: center;
+    border-top: 1px solid #2E405A;
+    gap: 16px;
+    padding-top: 16px;
+    .card__avatar {
+      display: flex;
+      border-radius: 90px;
+      border: 1px solid white;
+      img {
+        width: 33px;
+      }
+    }
+    .card__autor--alt-color {
+      color: white;
+      cursor: pointer;
+      &:hover {
+        color: #00FFF8;
+      }
+    }
+  }
+}
+
+//Transition
+.fade-enter-active,
+.fade-leave-active {
+    transition: 0.25s ease-out;
+}
+.fade-leave-to,
+.fade-enter-from {
+  opacity: 0;
+}
+</style>
